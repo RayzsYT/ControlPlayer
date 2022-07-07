027a4ad5744f8fe31c435dc8e4d7b4fcd0dfe781
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2022-07-07 22:41:22 +0200

    Fixed 1.18> Animation
    Fixed the animation for all versions above the 1.18

--- src/main/java/de/rayzs/controlplayer/api/packetbased/animation/impl/ModernArmSwingAnimation.java
+++ src/main/java/de/rayzs/controlplayer/api/packetbased/animation/impl/ModernArmSwingAnimation.java
@@ -1,17 +1,44 @@
 package de.rayzs.controlplayer.api.packetbased.animation.impl;
 
-import net.minecraft.network.protocol.game.PacketPlayOutAnimation;
-import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
 import de.rayzs.controlplayer.api.packetbased.animation.ArmSwing;
+import de.rayzs.controlplayer.api.version.ServerVersion;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.PacketPlayOutAnimation;
+import net.minecraft.network.protocol.game.PacketPlayOutBlockBreakAnimation;
 import net.minecraft.server.level.EntityPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class ModernArmSwingAnimation implements ArmSwing {
 
+    private final String versionName;
+    private boolean disableArmSwing = false;
+
+    public ModernArmSwingAnimation(String versionName) {
+        this.versionName = versionName;
+    }
+
     @Override
     public void execute(Player player) {
-        CraftPlayer craftPlayer = (CraftPlayer) player;
-        EntityPlayer entityPlayer = craftPlayer.getHandle();
-        entityPlayer.b.sendPacket(new PacketPlayOutAnimation(entityPlayer, 0));
+        if (disableArmSwing || player == null) return;
+        try {
+            Class<?> entityClass = Class.forName("net.minecraft.world.entity.Entity");
+            Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
+            Object animationPacket = Class.forName("net.minecraft.network.protocol.game.PacketPlayOutAnimation").getDeclaredConstructor(entityClass, int.class).newInstance(entityPlayer, 0);
+            Object playerConnection = entityPlayer.getClass().getField("b").get(entityPlayer);
+            if(versionName.contains("17")) playerConnection.getClass().getMethod("sendPacket", Class.forName("net.minecraft.network.protocol.game.Packet")).invoke(playerConnection, animationPacket);
+            else playerConnection.getClass().getMethod("a", Class.forName("net.minecraft.network.protocol.Packet")).invoke(playerConnection, animationPacket);
+        } catch (ClassNotFoundException
+                | NoSuchMethodException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchFieldException
+                | InstantiationException exception) {
+            System.out.println("Disabled ArmSwingAnimations because this version isn't is supported!");
+            disableArmSwing = true;
+        }
     }
 }
