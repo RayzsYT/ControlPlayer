a1a11b0876e5792e98bebf4de422fb139b7dd25c
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2022-08-14 13:10:31 +0200

    Fix Knockback and Damage
    Fixed that controlled player can be damaged, killed and knocked back

--- src/main/java/de/rayzs/controlplayer/plugin/events/EntityDamageByEntity.java
+++ src/main/java/de/rayzs/controlplayer/plugin/events/EntityDamageByEntity.java
@@ -1,11 +1,15 @@
 package de.rayzs.controlplayer.plugin.events;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import de.rayzs.controlplayer.api.control.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
+import org.bukkit.util.Vector;
 
 import java.lang.reflect.Field;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class EntityDamageByEntity implements Listener {
 
@@ -28,19 +32,79 @@ public class EntityDamageByEntity implements Listener {
                     Field field = clazz.getDeclaredField("damager");
                     field.setAccessible(true);
                     field.set(event, victim);
-                } catch (Exception exception) { exception.printStackTrace(); }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
                 if (event.getEntity() instanceof Player) {
                     player = (Player) event.getEntity();
-                    if((player.getHealth() - event.getDamage()) < 0.5) {
+                    if ((player.getHealth() - event.getDamage()) < 0.5) {
                         event.setCancelled(true);
                         player.damage(event.getDamage(), victim);
                     }
                 }
             }
-        } else if(event.getEntity() instanceof Player) {
-            player = (Player) event.getEntity();
-            instanceState = ControlManager.getInstanceState(player);
-            if(instanceState == 1) event.setCancelled(true);
         }
+
+        if(event.getEntity() instanceof Player) {
+            player = (Player) event.getEntity();
+            Player attacker = event.getDamager() instanceof Player ? (Player) event.getDamager() : null;
+            instanceState = ControlManager.getInstanceState(player);
+            if (instanceState == 1) {
+                ControlInstance controlInstance = ControlManager.getControlInstance(player);
+                Player controller = controlInstance.controller();
+                event.setCancelled(false);
+
+                double healthAfterDamage = (player.getHealth() - event.getDamage());
+                if(!event.isCancelled()) pushPlayer(controller, event.getDamager());
+
+                if (healthAfterDamage < 0.5) {
+                    event.setCancelled(true);
+                    player.setHealth(0);
+                } else {
+                    controller.setHealth(healthAfterDamage < 0.5 ? 0 : healthAfterDamage);
+                }
+            }
+        }
+    }
+
+    /*
+      ORIGINAL CODE: https://gist.github.com/IllusionTheDev/b770d8e2ffd7a7d80351686c5ea46963
+      I just changed some variables and add some checks
+     */
+    private void pushPlayer(Entity victim, Entity attacker) {
+        Vector vector = new Vector(0.0, -0.078375, 0.0);
+        if(attacker != null) {
+            ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+            Location victimLocation = victim.getLocation(),
+                    attackerLocation = attacker.getLocation();
+
+            double deltaX = victimLocation.getX() - attackerLocation.getX(),
+                    deltaZ = victimLocation.getZ() - attackerLocation.getZ(),
+                    y = 0.36075;
+
+            while (deltaX * deltaX + deltaZ * deltaZ < 1.0E-4) {
+                deltaX = (threadLocalRandom.nextDouble()) - threadLocalRandom.nextDouble() * 0.01;
+                deltaZ = (threadLocalRandom.nextDouble()) - threadLocalRandom.nextDouble() * 0.01;
+            }
+
+            double distance = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ);
+            deltaX = deltaX / distance * 0.40;
+            deltaZ = deltaZ / distance * 0.40;
+
+            if (attacker instanceof Player) {
+                Player attackerAsPlayer = (Player) attacker;
+                if (!attackerAsPlayer.isSneaking()) {
+                    double yaw = attackerLocation.getYaw() * Math.PI / 180;
+                    deltaX += -Math.sin(yaw) * 1.5;
+                    deltaZ = Math.cos(yaw) * 1.5;
+                    y += 0.1;
+                    deltaX *= 0.6;
+                    deltaZ *= 0.6;
+                }
+            }
+            vector = new Vector(deltaX, y, deltaZ);
+        }
+
+        victim.setVelocity(vector);
     }
 }
