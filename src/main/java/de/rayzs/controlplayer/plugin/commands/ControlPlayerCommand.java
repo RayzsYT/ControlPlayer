f5ac08c0ed39eea9aed3c9c2daff9920fcebdce3
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2022-08-24 22:34:59 +0200

    Improve Control-Stop
    Improved that u can just type '/cp' except of '/cp [name]' to delete the during control-session.

--- src/main/java/de/rayzs/controlplayer/plugin/commands/ControlPlayerCommand.java
+++ src/main/java/de/rayzs/controlplayer/plugin/commands/ControlPlayerCommand.java
@@ -23,38 +23,40 @@ public class ControlPlayerCommand extends MessageManager implements CommandExecu
             return true;
         }
 
-        if(args.length == 1) {
-            sendHelp = false;
-            Player player = (Player) sender;
-            int playerInstance = ControlManager.getInstanceState(player);
+        Player player = (Player) sender;
+        int playerInstance = ControlManager.getInstanceState(player);
 
-            switch (playerInstance) {
-                case 0:
-                    sender.sendMessage(ControlManager.deleteControlInstance(player) ? getMessage(MessageType.STOPPED_CONTROLLING) : getMessage(MessageType.ERROR));
-                    break;
-                case 1: default:
-                    sender.sendMessage(getMessage(MessageType.BEING_CONTROLLED));
-                    break;
-                case -1:
+        switch (playerInstance) {
+            case 0:
+                sender.sendMessage(ControlManager.deleteControlInstance(player) ? getMessage(MessageType.STOPPED_CONTROLLING) : getMessage(MessageType.ERROR));
+                return true;
+            case 1: default:
+                sender.sendMessage(getMessage(MessageType.BEING_CONTROLLED));
+                return true;
+            case -1:
+                if(args.length == 1) {
+                    sendHelp = false;
                     Player victim = Bukkit.getPlayer(args[0]);
-                    if(victim != null) {
+                    if (victim != null) {
                         int victimInstance = ControlManager.getInstanceState(victim);
-                        if(victim != player) {
+                        if (victim != player) {
                             if (!(victim.isOp() || victim.hasPermission("controlplayer.bypass"))) {
-                                if (victimInstance != 1) {
-                                    ControlState state = ControlManager.createControlInstance(player, victim);
-                                    if(state == ControlState.SUCCESS) sender.sendMessage(getMessage(MessageType.SUCCESS).replace("%player%", victim.getName()));
-                                    else sender.sendMessage(getMessage(MessageType.ERROR));
-                                } else sender.sendMessage(getMessage(MessageType.ALREADY_CONTROLLED));
+                                if (!victim.isDead()) {
+                                    if (victimInstance != 1) {
+                                        ControlState state = ControlManager.createControlInstance(player, victim);
+                                        if (state == ControlState.SUCCESS)
+                                            sender.sendMessage(getMessage(MessageType.SUCCESS).replace("%player%", victim.getName()));
+                                        else sender.sendMessage(getMessage(MessageType.ERROR));
+                                    } else sender.sendMessage(getMessage(MessageType.ALREADY_CONTROLLED));
+                                } else sender.sendMessage(getMessage(MessageType.NOT_ALIVE));
                             } else sender.sendMessage(getMessage(MessageType.PLAYER_IMUN));
                         } else sender.sendMessage(getMessage(MessageType.SELF_CONTROL));
-                    } else sender.sendMessage(getMessage(MessageType.ONLY_PLAYERS));
+                    } else sender.sendMessage(getMessage(MessageType.NOT_ONLINE));
                     break;
-            }
+                }
         }
 
         if(sendHelp) sender.sendMessage(getMessage(MessageType.USAGE));
-
         return true;
     }
 }
