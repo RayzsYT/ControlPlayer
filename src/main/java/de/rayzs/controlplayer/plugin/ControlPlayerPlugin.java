5d371e53531473c7b587211be9ac84add8a63933
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2022-08-25 21:31:41 +0200

    Improved Updater
    Improved the updater to prevent the server console from printing errors. 
    Simply just added a double-check. If the first URL could not be opened, the second one will.

--- src/main/java/de/rayzs/controlplayer/plugin/ControlPlayerPlugin.java
+++ src/main/java/de/rayzs/controlplayer/plugin/ControlPlayerPlugin.java
@@ -10,25 +10,34 @@ import org.bukkit.command.PluginCommand;
 import org.bukkit.event.Listener;
 import org.bukkit.Bukkit;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ControlPlayerPlugin extends JavaPlugin {
 
     private static ControlPlayerPlugin instance;
 
     private boolean latestVersion = true;
-    private final WebConnection web = new WebConnection();
     private int updaterTaskId;
+    private final WebConnection web = new WebConnection();
     private final String[] mainCommandNames = {"cp", "controlplayer", "cplayer", "controlp"},
-                           reloadCommandNames = {"controlplayerreload", "controlplayerr", "cpr", "cpreload"};
+            reloadCommandNames = {"controlplayerreload", "controlplayerr", "cpr", "cpreload"};
+
+    private final AtomicInteger urlId = new AtomicInteger();
+    private final String path = "https://www.rayzs.de/products/controlplayer/version/";
+    private final String[] urls = {
+            "index.php", // I can't completely switch to the new url bcs many people are using my old system and need the old system.
+            "raw/version.txt" // A backup page to get the current version.
+    };
 
     private final Class<?>[] listenerClasses = {
-                                                PlayerChangeWorld.class, PlayerDeath.class, PlayerInteract.class, PlayerInteractAtEntity.class, PlayerAnimation.class,
-                                                EntityDamage.class, EntityDamageByEntity.class, EntityTargetLivingEntity.class,
-                                                PlayerToggleFlight.class, PlayerToggleSneak.class, PlayerToggleSprint.class,
-                                                PlayerPickupItem.class, PlayerDropItem.class,
-                                                PlayerJoin.class, PlayerQuit.class,
-                                                BlockBreak.class, BlockPlace.class,
-                                                AsyncPlayerChat.class,
-                                                };
+            PlayerChangeWorld.class, PlayerDeath.class, PlayerInteract.class, PlayerInteractAtEntity.class, PlayerAnimation.class,
+            EntityDamage.class, EntityDamageByEntity.class, EntityTargetLivingEntity.class,
+            PlayerToggleFlight.class, PlayerToggleSneak.class, PlayerToggleSprint.class,
+            PlayerPickupItem.class, PlayerDropItem.class,
+            PlayerJoin.class, PlayerQuit.class,
+            BlockBreak.class, BlockPlace.class,
+            AsyncPlayerChat.class,
+    };
 
 
     @Override
@@ -86,19 +95,34 @@ public class ControlPlayerPlugin extends JavaPlugin {
         boolean update = updateObject == null || (boolean) updateObject;
         if(!update) return;
         Object delayObject = SettingsManager.getSetting(SettingType.UPDATER_DELAY);
-        int delay = delayObject == null ? 18000 : (int) delayObject;
+        int configDelay = delayObject == null ? 18000 : (int) delayObject;
+        startUpdaterScheduler(20, 20, configDelay);
+    }
+
+    private void startUpdaterScheduler(int startDelay, int delay, int configDelay) {
         updaterTaskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            String result = web.connect("https://www.rayzs.de/products/controlplayer/version/index.php").getResult();
-            if(!result.equals(getDescription().getVersion())) {
+            int id = urlId.get();
+
+            if(id == 1) getLogger().info("Switched to 'raw' page to check if the plugin is outdated.");
+            else if(id > 1) {
+                getLogger().warning("Failed reaching web host! (blocked by firewall? website down?)");
                 Bukkit.getScheduler().cancelTask(updaterTaskId);
-                if(result.equals("nohost")) getLogger().warning("Failed reaching web-host! (firewall?)");
-                else if(result.equals("exception")) getLogger().warning("Failed creating web-instance! (outdated java version?)");
-                else {
-                    getLogger().warning("You're using an outdated version of this plugin!");
-                    getLogger().warning("Please update it on: https://www.rayzs.de/products/controlplayer/page");
-                }
-                latestVersion = false;
+                return;
             }
-        }, 20, delay);
+
+            web.connect(path + urls[id]);
+            String result = web.getResult();
+
+            if(result.equals("unknown") || result.equals("exception")) urlId.addAndGet(1);
+            else if(!getDescription().getVersion().equals(result)) {
+                latestVersion = false;
+                Bukkit.getScheduler().cancelTask(updaterTaskId);
+                getLogger().warning("You're using an outdated version of this plugin!");
+                getLogger().warning("Please update it on: https://www.rayzs.de/products/controlplayer/page");
+            } else {
+                Bukkit.getScheduler().cancelTask(updaterTaskId);
+                startUpdaterScheduler(configDelay, configDelay, configDelay);
+            }
+        }, startDelay, delay);
     }
 }
