cfc4f209b270db52c0b73cb743698c4509b13f09
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2023-01-19 15:52:57 +0100

    Code improvement & Change class name

---
+++ src/main/java/de/rayzs/controlplayer/plugin/events/PlayerChat.java
@@ -0,0 +1,45 @@
+package de.rayzs.controlplayer.plugin.events;
+
+import de.rayzs.controlplayer.api.files.messages.*;
+import de.rayzs.controlplayer.plugin.ControlPlayerPlugin;
+import org.bukkit.Bukkit;
+import de.rayzs.controlplayer.api.files.settings.*;
+import de.rayzs.controlplayer.api.control.*;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.player.PlayerChatEvent;
+
+public class PlayerChat extends MessageManager implements Listener {
+
+    private final ControlPlayerPlugin instance;
+
+    public PlayerChat() {
+        this.instance = ControlPlayerPlugin.getInstance();
+    }
+
+    @EventHandler
+    public void onPlayerChat(PlayerChatEvent event) {
+        if(event.isCancelled()) return;
+
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+        int instanceState = ControlManager.getInstanceState(player);
+        Object forceChatObject = SettingsManager.getSetting(SettingType.CONTROL_RUNNING_FORCECHAT_ENABLED);
+        boolean forceChat = forceChatObject == null || (boolean) forceChatObject;
+
+        Object bypassMessageObject = SettingsManager.getSetting(SettingType.CONTROL_RUNNING_FORCECHAT_BYPASSMESSAGE);
+        String bypassMessage = bypassMessageObject == null ? "-b " : (String) bypassMessageObject;
+
+        if(!forceChat || instanceState != 0) return;
+
+        ControlInstance instance = ControlManager.getControlInstance(player);
+        if(message.toLowerCase().startsWith(bypassMessage)) {
+            message = message.replace(bypassMessage, "");
+            event.setMessage(message);
+        } else {
+            Player victim = instance.victim();
+            event.setCancelled(true);
+            victim.chat(event.getMessage());
+        }
+    }
+}
