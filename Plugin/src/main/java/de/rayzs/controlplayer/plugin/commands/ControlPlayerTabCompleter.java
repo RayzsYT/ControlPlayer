6d20a3d230ad0989a8cb15e1207149fbe4380eb0
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-10-17 00:27:22 +0200

    Add Hierarchy-System

--- Plugin/src/main/java/de/rayzs/controlplayer/plugin/commands/ControlPlayerTabCompleter.java
+++ Plugin/src/main/java/de/rayzs/controlplayer/plugin/commands/ControlPlayerTabCompleter.java
@@ -1,19 +1,30 @@
 package de.rayzs.controlplayer.plugin.commands;
 
-import de.rayzs.controlplayer.api.files.settings.SettingType;
-import de.rayzs.controlplayer.api.files.settings.SettingsManager;
-import org.bukkit.Bukkit;
+import de.rayzs.controlplayer.api.hierarchy.HierarchyManager;
+import de.rayzs.controlplayer.api.files.settings.*;
+import org.bukkit.entity.Player;
 import org.bukkit.command.*;
+import org.bukkit.Bukkit;
 import java.util.*;
 
 public class ControlPlayerTabCompleter {
 
     public static List<String> getTabCompletion(CommandSender sender) {
         List<String> results = new ArrayList<>();
+
+        if(!(sender instanceof Player)) return results;
+        Player executor = (Player) sender;
+
         if(sender.isOp() || sender.hasPermission("controlplayer.use"))
             Bukkit.getOnlinePlayers().stream().filter(player -> {
                 if(player.getName().equals(sender.getName())) return false;
-                return (boolean) SettingsManager.getSetting(SettingType.SYSTEM_IGNOREBYPASS) || !(player.isOp() || player.hasPermission("controlplayer.bypass"));
+
+                if(!(boolean) SettingsManager.getSetting(SettingType.SYSTEM_IGNOREBYPASS)) {
+                    if(player.isOp() || HierarchyManager.isHigher(player, executor))
+                        return false;
+                }
+
+                return true;
             }).forEach(player
                     -> results.add(player.getName()));
         return results;
