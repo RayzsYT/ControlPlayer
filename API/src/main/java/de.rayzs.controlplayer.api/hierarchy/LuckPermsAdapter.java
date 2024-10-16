08c80f84f1ad24b53a0de37693cde0959e9fe804
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-10-17 00:06:32 +0200

    Add LuckPerms integration for Hierarchy-System

---
+++ API/src/main/java/de.rayzs.controlplayer.api/hierarchy/LuckPermsAdapter.java
@@ -0,0 +1,29 @@
+package de.rayzs.controlplayer.api.hierarchy;
+
+import net.luckperms.api.cacheddata.CachedDataManager;
+import net.luckperms.api.model.user.*;
+import java.util.stream.Collectors;
+import org.bukkit.entity.Player;
+import net.luckperms.api.*;
+import java.util.*;
+
+public class LuckPermsAdapter {
+
+    private final LuckPerms provider;
+    private final UserManager manager;
+
+    public LuckPermsAdapter() {
+        provider = LuckPermsProvider.get();
+        manager = provider.getUserManager();
+    }
+
+    List<String> getHierarchyPerms(Player player) {
+        List<String> result = new ArrayList<>();
+        User user = manager.getUser(player.getName());
+        if(user == null) return result;
+
+        CachedDataManager data = user.getCachedData();
+        result = data.getPermissionData().getPermissionMap().keySet().stream().filter(perms -> perms.startsWith("controlplayer.hierarchy.")).collect(Collectors.toList());
+        return result;
+    }
+}
