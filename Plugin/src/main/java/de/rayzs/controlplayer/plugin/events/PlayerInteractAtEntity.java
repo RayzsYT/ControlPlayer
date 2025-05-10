8ede2047bfa4502b7b5599a1b9ba2daf9c39ce48
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2025-05-11 01:53:00 +0200

    Add control over tamed animals

--- Plugin/src/main/java/de/rayzs/controlplayer/plugin/events/PlayerInteractAtEntity.java
+++ Plugin/src/main/java/de/rayzs/controlplayer/plugin/events/PlayerInteractAtEntity.java
@@ -1,21 +1,83 @@
 package de.rayzs.controlplayer.plugin.events;
 
+import de.rayzs.controlplayer.api.packetbased.animation.ArmSwingAnimation;
+import de.rayzs.controlplayer.api.utils.ExpireList;
+import org.bukkit.entity.*;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import de.rayzs.controlplayer.api.control.*;
-import org.bukkit.entity.Player;
 import org.bukkit.event.*;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
-public class PlayerInteractAtEntity implements Listener {
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class PlayerInteractAtEntity extends ArmSwingAnimation implements Listener {
+
+    private final ExpireList<Player> delayedPlayers = new ExpireList<>(10, TimeUnit.MILLISECONDS);
 
     @EventHandler (priority = EventPriority.LOWEST)
     public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
         Player player = event.getPlayer();
         ControlInstance instance = ControlManager.getControlInstance(player);
-        if(instance == null) return;
+        if(instance == null)
+            return;
 
         ControlSwap swap = ControlManager.getControlSwap(instance);
         int instanceState = ControlManager.getInstanceState(player);
         boolean useSwap = swap.isEnabled() && swap.isSwapped();
-        if (useSwap && instanceState == 0 || !useSwap && instanceState == 1) event.setCancelled(true);
+        if (useSwap && instanceState == 0 || !useSwap && instanceState == 1)
+            event.setCancelled(true);
+    }
+
+    @EventHandler (priority = EventPriority.LOWEST)
+    public void onPlayerInteractAtEntity(PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer();
+        ControlInstance instance = ControlManager.getControlInstance(player);
+        if(instance == null)
+            return;
+
+        ControlSwap swap = ControlManager.getControlSwap(instance);
+        int instanceState = ControlManager.getInstanceState(player);
+        boolean useSwap = swap.isEnabled() && swap.isSwapped();
+        if (useSwap && instanceState == 0 || !useSwap && instanceState == 1) {
+            event.setCancelled(true);
+            return;
+        }
+
+        if (useSwap && instanceState == 1 || !useSwap && instanceState == 0) {
+
+            if (event.getRightClicked() instanceof Sittable && event.getRightClicked() instanceof Tameable) {
+                Entity clickedEntity = event.getRightClicked();
+
+                Tameable tameable = (Tameable) clickedEntity;
+                Sittable sittable = (Sittable) clickedEntity;
+
+                if (!tameable.isTamed() || tameable.getOwner() == null || tameable.getOwner().getName() == null) {
+                    return;
+                }
+
+                ControlInstance controlInstance = ControlManager.getControlInstance(player);
+                if (controlInstance == null) {
+                    return;
+                }
+
+                if (!tameable.getOwner().getName().equalsIgnoreCase(controlInstance.victim().getName())) {
+                    return;
+                }
+
+                if (delayedPlayers.contains(player)) {
+                    return;
+                }
+
+                delayedPlayers.addIgnoreIfContains(player);
+
+                sittable.setSitting(!sittable.isSitting());
+
+                execute(instance.victim(), "");
+                execute(instance.controller(), "");
+
+            }
+
+        }
     }
 }
